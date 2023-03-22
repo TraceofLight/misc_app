e7af529812781c25f7c39e54daca75070f145502
TraceofLight <rickyjun45@gmail.com>
2023-03-23 00:16:30 +0900

    Test: Get Image from Request
    요청에서 받은 이미지를 활용하는 방식 테스트

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import Response
 
+import os
+import tempfile
 import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
 app = FastAPI()
 
 my_model = tf.keras.models.load_model("model")
 
-def make_result_with_model():
-    content_image = plt.imread("asset/profile.jpg")
+def make_result_with_model(image_path):
+    content_image = plt.imread(image_path)
     style_image = plt.imread("asset/target.jpg")
     content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
     style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
@@ -24,9 +27,24 @@ def make_result_with_model():
 
     return encoded_image
 
+async def get_image(file: UploadFile):
+
+    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file
+        tmp_file.write(await file.read())
+        tmp_file.flush()
+        tmp_file.seek(0)
+
+        image = Image.open(tmp_file.name)
+        image = image.convert("RGB")
+        save_path = os.path.join(tempfile.gettempdir(), f"{file.filename}.jpg")
+        image.save(save_path, format="JPEG")
+
+        os.unlink(tmp_file.name)
+
+    return save_path
+
 @app.post("/makeImage")
 async def make_image(file: UploadFile):
-
-    img_byte = make_result_with_model()
+    img_byte = make_result_with_model(get_image(file))
 
     return Response(content=img_byte.numpy(), media_type="image/jpeg")
