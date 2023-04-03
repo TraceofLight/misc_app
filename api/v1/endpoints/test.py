6806a1d73346674567eb0f64a2883717afc6f563
TraceofLight <rickyjun45@gmail.com>
2023-04-03 15:37:27 +0900

    Feat: New Model
    적용할 모델을 반영한 새로운 API 적용

--- api/v1/endpoints/test.py
+++ api/v1/endpoints/test.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter, Response
 router = APIRouter()
 
 @router.get("/api/v1/itsGrey/test")
-async def hello_world():
+async def test_request():
     return Response("Your Request Has Arrived Successfully!")
